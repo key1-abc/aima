9f407c24bbefa6a12c7e9ae77d82d9df8e3f16aa
key1-abc <keyvanabbasi282@gmail.com>
2025-12-26 17:31:33 +0330

    Update ai-hw2.py

--- ai-hw2.py
+++ ai-hw2.py
@@ -1,70 +1,104 @@
 from abc import ABC, abstractmethod
+from typing import List, Tuple, Any, Optional
 
 class State(ABC):
     @abstractmethod
-    def __hash__(self):
+    def __eq__(self, other: 'State') -> bool:
         pass
-
+    
     @abstractmethod
-    def __eq__(self, other):
+    def __hash__(self) -> int:
         pass
-
+    
     @abstractmethod
-    def __repr__(self):
+    def __str__(self) -> str:
         pass
 
-class Problem(ABC):
-    def __init__(self, initial_state):
-        self.initial_state = initial_state
-
+class Problem(ABC):    
     @abstractmethod
-    def goal_test(self, state):
+    def get_initial_state(self) -> State:
         pass
-
+    
     @abstractmethod
-    def successors(self, state):
+    def is_goal_state(self, state: State) -> bool:
         pass
-
-    def result(self, state, action):
-        return action
+    
+    @abstractmethod
+    def get_successors(self, state: State) -> List[Tuple[Any, State]]:
+        pass
+    
+    @abstractmethod
+    def get_goal_state(self) -> State:
+        pass
+    
+    def get_cost(self, state1: State, action: Any, state2: State) -> float:
+        return 1.0
 
 class TowerOfHanoiState(State):
-    def __init__(self, pegs):
-        self.pegs = tuple(tuple(peg) for peg in pegs)
-
-    def __hash__(self):
-        return hash(self.pegs)
-
-    def __eq__(self, other):
+   
+    def __init__(self, pegs: List[List[int]]):
+        self.pegs = [peg.copy() for peg in pegs]  
+        self.num_disks = sum(len(peg) for peg in pegs)
+    
+    def __eq__(self, other: 'TowerOfHanoiState') -> bool:
+        if not isinstance(other, TowerOfHanoiState):
+            return False
         return self.pegs == other.pegs
+    
+    def __hash__(self) -> int:
+        return hash((tuple(tuple(peg) for peg in self.pegs)))
+    
+    def __str__(self) -> str:
+        return f"TowerOfHanoiState(pegs={self.pegs})"
+    
+    def __repr__(self) -> str:
+        return self.__str__()
+    
+    def can_move(self, from_peg: int, to_peg: int) -> bool:
+        if not (0 <= from_peg < 3 and 0 <= to_peg < 3):
+            return False
+        if not self.pegs[from_peg]:
+            return False
+        if not self.pegs[to_peg]:
+            return True
+        return self.pegs[from_peg][-1] < self.pegs[to_peg][-1]
+    
+    def move_disk(self, from_peg: int, to_peg: int) -> 'TowerOfHanoiState':
+        if not self.can_move(from_peg, to_peg):
+            raise ValueError("Invalid move")
+        
+        new_pegs = [peg.copy() for peg in self.pegs]
+        disk = new_pegs[from_peg].pop()
+        new_pegs[to_peg].append(disk)    
+        return TowerOfHanoiState(new_pegs)
 
-    def __repr__(self):
-        return str(self.pegs)
-
-class TowerOfHanoi(Problem):
-    def __init__(self, num_disks):
-        initial_state = TowerOfHanoiState((tuple(range(num_disks, 0, -1)), (), ()))
-        super().__init__(initial_state)
+class TowerOfHanoiProblem(Problem):    
+    def __init__(self, num_disks: int = 3):
         self.num_disks = num_disks
-        self.goal_state = TowerOfHanoiState(((), (), tuple(range(num_disks, 0, -1))))
-
-    def goal_test(self, state):
-        return state == self.goal_state
-
-    def successors(self, state):
+        initial_pegs = [list(range(num_disks, 0, -1)), [], []]
+        self._initial_state = TowerOfHanoiState(initial_pegs)
+        goal_pegs = [[], [], list(range(num_disks, 0, -1))]
+        self._goal_state = TowerOfHanoiState(goal_pegs)
+    
+    def get_initial_state(self) -> TowerOfHanoiState:
+        return self._initial_state
+    
+    def get_goal_state(self) -> TowerOfHanoiState:
+        return self._goal_state
+    
+    def is_goal_state(self, state: TowerOfHanoiState) -> bool:
+        return state == self._goal_state
+    
+    def get_successors(self, state: TowerOfHanoiState) -> List[Tuple[Tuple[int, int], TowerOfHanoiState]]:
         successors = []
         for from_peg in range(3):
-            if not state.pegs[from_peg]:
-                continue
             for to_peg in range(3):
-                if from_peg == to_peg:
-                    continue
-                if state.pegs[to_peg] and state.pegs[to_peg][-1] < state.pegs[from_peg][-1]:
-                    continue
-                new_pegs = [list(peg) for peg in state.pegs]
-                disk = new_pegs[from_peg].pop()
-                new_pegs[to_peg].append(disk)
-                new_state = TowerOfHanoiState(new_pegs)
-                action = (from_peg, to_peg)
-                successors.append((action, new_state))
+                if from_peg != to_peg and state.can_move(from_peg, to_peg):
+                    new_state = state.move_disk(from_peg, to_peg)
+                    action = (from_peg, to_peg)
+                    successors.append((action, new_state))
         return successors
+    
+    def heuristic(self, state: TowerOfHanoiState) -> float:
+        disks_on_goal = len(state.pegs[2])
+        return self.num_disks - disks_on_goal
